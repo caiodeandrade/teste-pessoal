dd520c8b58011359b6cdd5cc5291387afaaf270b
caio-andrade7 <caio.andrade@7comm.com.br>
2025-02-26 00:41:12 -0300

    padding

--- teste.dart
+++ teste.dart
@@ -1,4 +1,16 @@
-void main() {
-  int meu_numero = 42;
-  print(meu_numero)
+import 'package:flutter/material.dart';
+
+class PaddingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: ElevatedButton(
+          onPressed: () {},
+          child: const Text('Clique Aqui'),
+        ),
+      ),
+    );
+  }
 }
